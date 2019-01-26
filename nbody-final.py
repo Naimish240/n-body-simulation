0e26176701a6fabdf8ac04c8b9e489cc830ccebd
Naimish <naimish240@gmail.com>
2019-01-27 01:05:28 +0530

    commiting to master stuff done on local
    pushing files from desktop to the master

---
+++ nbody-final.py
@@ -0,0 +1,177 @@
+from math import sqrt
+from random import choice
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from datetime import datetime
+from copy import deepcopy
+from os import mkdir
+
+# --------------------------------- Class Point ---------------------------------
+class Point(object):
+    def __init__(self, x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return '({},{},{})'.format(self.x,self.y,self.z)
+    
+# --------------------------------- Class Body ----------------------------------
+class Body(object):
+    # location: meters, mass: kilograms, velocity: meters per second
+    def __init__(self, posn = Point(0,0,0), m = 0, v = Point(0,0,0), name = ""):
+        self.posn = posn
+        self.m = m
+        self.v = v
+        self.name = name
+    
+    def __repr__(self):
+        s = []
+        s.append("Name: {}\n".format(self.name))
+        s.append("Mass: {}\n".format(self.m))
+        s.append("Initial Position: {}\n".format(str(self.posn)))
+        s.append("Initial Velocity: {}\n".format((str(self.v))))
+
+        return s[0]+s[1]+s[2]+s[3]
+
+# Function to calculate the acceleration of the body
+def calAccn(bodies, i):
+    G = 6.67408e-11 # Unit: m3 kg-1 s-2
+    accn = Point(0,0,0)
+    current = bodies[i]
+    for j, other in enumerate(bodies):
+        if j != i:
+            r = (current.posn.x - other.posn.x)**2 + (current.posn.y - other.posn.y)**2 + (current.posn.z - other.posn.z)**2
+            r = sqrt(r)
+            tmp = G * other.m / r**3
+            accn.x += tmp * (other.posn.x - current.posn.x)
+            accn.y += tmp * (other.posn.y - current.posn.y)
+            accn.z += tmp * (other.posn.z - current.posn.z)
+
+    return accn
+
+# Function to calculate the velocity of the body
+def calVelo(bodies, dt):
+    for i, current in enumerate(bodies):
+        accn = calAccn(bodies, i)
+
+        current.v.x += accn.x * dt
+        current.v.y += accn.y * dt
+        current.v.z += accn.z * dt 
+
+# Function to find the updated location of the body
+def newLocn(bodies, dt):
+    for i in bodies:
+        i.posn.x += i.v.x * dt
+        i.posn.y += i.v.y * dt
+        i.posn.z += i.v.z * dt
+
+# Function to plot bodies
+def plot(bodies, outfile):
+    fig = plt.figure()
+    colours = ['r','b','g','y','m','c']
+    ax = fig.add_subplot(1,1,1, projection='3d')
+    max_range = 0
+    for current_Body in bodies: 
+        max_dim = max(max(current_Body["x"]),max(current_Body["y"]),max(current_Body["z"]))
+        if max_dim > max_range:
+            max_range = max_dim
+        ax.plot(current_Body["x"], current_Body["y"], current_Body["z"], c = choice(colours), label = current_Body["name"])        
+    
+    ax.set_xlim([-max_range,max_range])    
+    ax.set_ylim([-max_range,max_range])
+    ax.set_zlim([-max_range,max_range])
+    ax.legend()        
+
+    if outfile:
+        plt.savefig(outfile)
+    else:
+        plt.show()
+
+# Function to run the simulation
+def run(bodies, dt , steps , report_freq):
+
+    #create output container for each Body
+    Body_locations_hist = []
+    for current_Body in bodies:
+        Body_locations_hist.append({"x":[], "y":[], "z":[], "name":current_Body.name})
+        
+    for i in range(1,steps):
+        calVelo(bodies, dt = dt)
+        newLocn(bodies, dt = dt)           
+        
+        if i % report_freq == 0:
+            for i, Body_location in enumerate(Body_locations_hist):
+                Body_location["x"].append(bodies[i].posn.x)
+                Body_location["y"].append(bodies[i].posn.y)           
+                Body_location["z"].append(bodies[i].posn.z)       
+
+    return Body_locations_hist        
+
+# Function to log the data onto a text file
+def log(bodies, motions, outfile):
+
+    with open(outfile, 'w') as fh:
+        l = []
+        for i in bodies:
+            l.append(str(i))
+        fh.writelines(l)
+        
+        for i in motions:
+            pts = []
+            for j in range(len(i['x'])):
+                pts.append((i['x'][j],i['y'][j],i['z'][j]))
+            fh.writelines([i['name'],'\n',str(pts),'\n'])
+
+# Function to get input of bodies with while loop
+def getInput():
+    l = []
+    while True:
+        name = input("Enter the name of the body or 0 to quit: ")
+        if '0' in name or not name:
+            return l
+        
+        px = float(input("Enter the body's initial 'x' co ordinate: "))
+        py = float(input("Enter the body's initial 'y' co ordinate: "))
+        pz = float(input("Enter the body's initial 'z' co ordinate: "))
+        posn = Point(px, py, pz)
+
+        m = float(input("Enter the mass of the body: "))
+
+        vx = float(input("Enter the body's initial 'x' velocity: "))
+        vy = float(input("Enter the body's initial 'y' velocity: "))
+        vz = float(input("Enter the body's initial 'z' velocity: "))
+        v = Point(vx, vy, vz)
+
+        l.append(Body(posn, m, v, name))
+
+
+if __name__ == "__main__":
+
+    #build list of bodies in the simulation
+    body = [
+        Body( posn = Point(0,0,0), m = 5e10, v = Point(0,0,0), name = "Body1" ),
+        Body( posn = Point(0,1e5,0), m = 5e10, v = Point(0,0,0), name = "Body2" ),
+        Body( posn = Point(0,0,1e5), m = 5e10, v = Point(0,0,0), name = "Body3" ),
+        ]
+    
+    '''body = getInput()'''
+
+    bodies = deepcopy(body)
+
+    steps = int(input("Enter the total number of steps: "))    
+    dt = float(input("Enter how large you want the time interval between steps to be: "))
+    report = int(input("Enter the number of steps between each log: "))
+
+    print("Running the simulation...")
+    motions = run(bodies, dt, steps, report)
+
+    time = datetime.now().strftime("%y-%m-%d-%H-%M")
+    
+    mkdir(r'run_{}'.format(time))
+
+    print("Saving the log...")
+    log(body, motions, outfile = r'run_{}/orbits_{}.txt'.format(time, time))
+
+    print("Saving the plot...")
+    plot(motions, outfile = r'run_{}/orbits_{}.png'.format(time, time))
